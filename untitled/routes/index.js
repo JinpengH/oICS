0820eeae69a56ac1040100b0d895fb2612bfcbcf
lofnheart <jinpengh@usc.edu>
2019-01-16 22:33:31 -0800

    add signup page

--- untitled/routes/index.js
+++ untitled/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Login' });
+    res.render('index', { title: 'Login' });
+});
+
+router.get('/main', function(req, res, next) {
+    res.render('main', { title: 'mainpage' });
+});
+
+router.get('/signup', function(req, res, next) {
+    res.render('signup', { title: 'signup' });
 });
 
 module.exports = router;
